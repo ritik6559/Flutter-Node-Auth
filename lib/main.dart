b89951042d3e5dfae5a4ce5faab65dff10f67d6f
Ritik6559 <ritikjoshi741@gmail.com>
2024-09-26 01:01:14 +0530

    adding signout and done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_node_auth/providers/user_provider.dart';
+import 'package:flutter_node_auth/screens/home_screen.dart';
 import 'package:flutter_node_auth/screens/sign_up_screen.dart';
+import 'package:flutter_node_auth/services/auth_services.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -16,19 +18,29 @@ void main() {
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final AuthServices authServices = AuthServices();
+
+  @override
+  void initState() {
+    super.initState();
+    authServices.getUserData(context);
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Node Auth',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        useMaterial3: true
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: true),
       debugShowCheckedModeBanner: false,
-      home: const SignupScreen(),
+      home: Provider.of<UserProvider>(context).user.token.isEmpty ? const SignupScreen() : const HomeScreen(),
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,15 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_node_auth/providers/user_provider.dart';
+import 'package:flutter_node_auth/services/auth_services.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  void signOutUser(BuildContext context) {
+    AuthServices().signOut(context);
+  }
+
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<UserProvider>(context).user;
+
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: const [
-          Text('Yes'),
+        children: [
+          Text(user.id),
+          Text(user.email),
+          Text(user.name),
+          ElevatedButton(
+            onPressed: () => signOutUser(context),
+            style: ButtonStyle(
+              backgroundColor: WidgetStateProperty.all(Colors.blue),
+              textStyle: WidgetStateProperty.all(
+                const TextStyle(color: Colors.white),
+              ),
+              minimumSize: WidgetStateProperty.all(
+                Size(MediaQuery.of(context).size.width / 2.5, 50),
+              ),
+            ),
+            child: const Text(
+              "Sign Out",
+              style: TextStyle(color: Colors.white, fontSize: 16),
+            ),
+          ),
         ],
       ),
     );
